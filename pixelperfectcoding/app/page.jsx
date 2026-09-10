44c187f44c34b7121173ea104fb34ff5f662ce42
Santosh Kumar <santosh.kumar@BOLDINDIA.COM>
2026-09-10 15:40:34 +0530

    first commit 10 sept

--- pixelperfectcoding/app/page.jsx
+++ pixelperfectcoding/app/page.jsx
@@ -3,7 +3,7 @@ import HomePage from '../src/site-pages/HomePage'
 import { projects, services, stacks } from '../src/data/portfolioData'
 
 export const metadata = {
-  title: 'Frontend Engineering. Your Frontend Consulting Partner & Web Development',
+  title: 'Frontend Engineering, Your Frontend Consulting Partner & Web Development',
   description:
     'Build faster, scalable digital experiences with frontend engineering, UI/UX design, e-commerce, and performance expertise.',
 }
